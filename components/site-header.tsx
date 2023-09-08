dec75c4f9566472021bef3fde49376b53a0b2be1
jahzlariosa <dev@josephlariosa.com>
2023-09-09 07:23:24 +0800

    UI

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -12,7 +12,7 @@ import { options } from "@/app/api/auth/[...nextauth]/options"
 export async function SiteHeader() {
   const session = await getServerSession(options)
   return (
-    <header className="bg-background sticky top-0 z-40 w-full border-b">
+    <header className="bg-white sticky top-0 z-40 w-full border-b">
       <div className="px-4 flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
         <MainNav items={siteConfig.mainNav} />
         <MobileNav items={siteConfig.mobileNav}/>
