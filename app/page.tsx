8817bd8ab69f781d0fc8c5c427d8913d13c38d0a
jahzlariosa <dev@josephlariosa.com>
2023-09-09 07:53:29 +0800

    UI

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,11 @@ import PostList from "./data/postlist";
 export default function Home() {
   return (
    <>
-   <PostList endPoint="posts" pageSize={2}/>
+   <div className="container my-8 mx-auto">
+    <div className="flex">
+      <PostList endPoint="posts" pageSize={2}/>
+    </div>
+   </div>
    </>
   )
 }
