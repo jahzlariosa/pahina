00a6fccdb5f1696f1088c3cb1ee620ee17bf8c14
jahzlariosa <dev@josephlariosa.com>
2023-09-09 11:22:26 +0800

    AutoClose - Outside

--- components/navigation/mobile-nav.tsx
+++ components/navigation/mobile-nav.tsx
@@ -1,6 +1,6 @@
 'use client';
 import Link from 'next/link';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import { cn } from "@/lib/utils"
 import { NavItem } from '@/types/nav';
@@ -11,8 +11,9 @@ interface MainNavProps {
   items?: NavItem[]
 }
 
-export function MobileNav({ items }: MainNavProps) {
+export function MobileNav({ items }: MainNavProps): JSX.Element {
   const [isOpen, setIsOpen] = useState(false);
+  const navRef = useRef<HTMLDivElement | null>(null);
 
   const toggleNav = () => {
     setIsOpen(!isOpen);
@@ -22,6 +23,23 @@ export function MobileNav({ items }: MainNavProps) {
     setIsOpen(false);
   };
 
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (navRef.current && !navRef.current.contains(event.target as Node)) {
+        closeNav();
+      }
+    };
+
+    if (isOpen) {
+      document.addEventListener('click', handleClickOutside);
+    } else {
+      document.removeEventListener('click', handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, [isOpen]);
   return (
     <>
       <div className="flex flex-1 items-center justify-end w-full space-x-4">
@@ -29,16 +47,17 @@ export function MobileNav({ items }: MainNavProps) {
           <FaBars size={24} />
         </button>
         <div
-          className={cn(`fixed top-0 left-0 h-full w-64 transition-all transform  ${
-             isOpen ? 'translate-x-[-6.5%]' : '-translate-x-[120%]'} ease-in-out duration-300 md-hidden`)}
+          ref={navRef}
+          className={cn(`fixed top-0 left-0 h-full w-64 transition-all transform  ${isOpen ? 'translate-x-[-6.5%]' : '-translate-x-[120%]'} ease-in-out duration-300 md-hidden`)}
         >
+
           <div className="block md:hidden h-screen w-auto p-4 dark:bg-black bg-white transition-all duration-200 border-r border-stone-200">
-          <button className={`absolute right-[-1em] dark:hover:bg-black dark:bg-stone-800 bg-white hover:bg-black hover:text-white rounded-md border-stone-300 border p-1 transition-all duration-200 flex justify-end md:hidden`} onClick={toggleNav}>
-            <FaTimes size={24} />
-          </button>
+            <button className={`absolute right-[-1em] dark:hover:bg-black dark:bg-stone-800 bg-white hover:bg-black hover:text-white rounded-md border-stone-300 border p-1 transition-all duration-200 flex justify-end md:hidden`} onClick={toggleNav}>
+              <FaTimes size={24} />
+            </button>
             <div className="logo-mobile-nav mb-4">
               <Link href="/" className="flex items-center space-x-2">
-                <Icons.logo/>
+                <Icons.logo />
                 <span className="inline-block font-bold">{siteConfig.name}</span>
               </Link>
             </div>
@@ -63,7 +82,7 @@ export function MobileNav({ items }: MainNavProps) {
             ) : null}
           </div>
         </div>
-      </div >
+      </div>
     </>
   );
 }
