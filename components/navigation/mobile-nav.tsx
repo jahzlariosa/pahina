f1d4e98c6f4774d718eb6c8e5a9c2fdaf805e9e3
jahzlariosa <dev@josephlariosa.com>
2023-09-09 11:10:05 +0800

    Mobile Toggler Adjustments

--- components/navigation/mobile-nav.tsx
+++ components/navigation/mobile-nav.tsx
@@ -25,14 +25,17 @@ export function MobileNav({ items }: MainNavProps) {
   return (
     <>
       <div className="flex flex-1 items-center justify-end w-full space-x-4">
-        <button className="flex justify-end md:hidden" onClick={toggleNav}>
-          {isOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
+        <button className={`${isOpen ? 'translate-y-[-200%]' : '-translate-y-[0%]'} transition-all duration-200 flex justify-end md:hidden`} onClick={toggleNav}>
+          <FaBars size={24} />
         </button>
         <div
           className={cn(`fixed top-0 left-0 h-full w-64 transition-all transform  ${
              isOpen ? 'translate-x-[-6.5%]' : '-translate-x-[120%]'} ease-in-out duration-300 md-hidden`)}
         >
           <div className="block md:hidden h-screen w-auto p-4 dark:bg-black bg-white transition-all duration-200 border-r border-stone-200">
+          <button className={`absolute right-[-1em] dark:hover:bg-black dark:bg-stone-800 bg-white hover:bg-black  hover:text-white rounded-md border-stone-300 border p-1 transition-all duration-200 flex justify-end md:hidden`} onClick={toggleNav}>
+            <FaTimes size={24} />
+          </button>
             <div className="logo-mobile-nav mb-4">
               <Link href="/" className="flex items-center space-x-2">
                 <Icons.logo/>
