ca0227fb6e7685a0b07774eca96615de41997ae6
jahzlariosa <dev@josephlariosa.com>
2023-09-09 12:26:09 +0800

    UI

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
    <>
    <div className="container my-8 mx-auto max-w-6xl px-4">
-    <div className="flex flex-col">
+    <div className="flex flex-col space-y-4">
       <PostList endPoint="posts" pageSize={2}/>
     </div>
    </div>
